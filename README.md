c928e57728e8ae0da782d5fca6a15996d11d5496
Ying <yinglu2641@gmail.com>
2023-06-23 07:36:30 -0400

    html and css working with image and sound

---
+++ README.md
@@ -0,0 +1,3 @@
+# drumkit
+# Musical game developed using HTML, CSS, and JavaScript uses the NodeJS environment.
+# Press the keyboard letter coressponding with the icon on the page and listen to the different notes played on the drum. 
---
+++ index.js
@@ -0,0 +1,67 @@
+
+//dectect button clicks
+let numOfBtns = document.querySelectorAll('button')
+
+for (let i = 0; i < numOfBtns.length; i++) {
+        numOfBtns[i].addEventListener('click', function () {
+            let buttonInnerHTML = this.innerHTML
+
+            makeSound(buttonInnerHTML)
+            buttonAnimation(buttonInnerHTML)
+        })
+}
+//Dect Keyboard Press
+document.addEventListener('keydown', function(event) {
+    makeSound(event.key)
+
+    buttonAnimation(event.key)
+})
+
+//creates sound for the key pressed
+function makeSound(key) {
+            switch (key) {
+              case "w":
+                const tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+              case "a":
+                const tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+              case "s":
+                const tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+              case "d":
+                const tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+              case "j":
+                const snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+              case "k":
+                const crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+              case "l":
+                const kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+              default:
+                console.log(buttonInnerHTML);
+            }
+    
+} 
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector('.' + currentKey)
+    
+    activeButton.classList.add('pressed');
+
+    setTimeout(function () {
+        activeButton.classList.remove('pressed')
+    }, 100)
+
+}
